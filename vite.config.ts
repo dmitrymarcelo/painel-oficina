d11ee3d5277d79cf9fea3eed82e5c78d70603658
dmitrymarcelo <54320459+dmitrymarcelo@users.noreply.github.com>
2026-01-26 12:20:21 -0400

    dfs
    sdf

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // No Google Cloud, a aplicação geralmente roda na raiz. 
-  // Se estiver usando GitHub Pages, mude de volta para '/nome-do-repo/'
+  // Definido como '/' para deploy na raiz do domínio Netlify
   base: '/', 
   build: {
     outDir: 'dist',
