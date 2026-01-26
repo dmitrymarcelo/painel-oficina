6448f4bd5cfef7d6b7b230b2f3d7ec53a8288e66
dmitrymarcelo <54320459+dmitrymarcelo@users.noreply.github.com>
2026-01-26 12:24:15 -0400

    sdf
    sdf

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Definido como '/' para deploy na raiz do domínio Netlify
-  base: '/', 
+  // 'base: ./' garante que os assets sejam carregados corretamente no GitHub Pages
+  base: './', 
   build: {
     outDir: 'dist',
     emptyOutDir: true,
